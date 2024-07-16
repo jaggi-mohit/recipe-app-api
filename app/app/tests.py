6e2e130545de9ee3e9a38c485cc523a43ed2e77c
Mohit Singh Jaggi <jaggimohit01@gmail.com>
2024-07-16 17:04:08 +0530

    resolve errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,9 +4,9 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
-        res=calc.add(5, 6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res=calc.subtract(15, 4)
+        res = calc.subtract(15, 4)
         self.assertEqual(res, 11)
